df29f17d7085b8807134f4ed2435e77bcef1417e
Nicolas <nicolas.marsil02@gmail.com>
2025-05-14 17:08:35 -0300

    feat: all repositories created

---
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/repository/LeituraRepository.java
@@ -0,0 +1,9 @@
+package com.github.nmsilos.backendtcc.repository;
+
+import com.github.nmsilos.backendtcc.models.Leitura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LeituraRepository extends JpaRepository<Leitura, Long> {
+}
---
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.github.nmsilos.backendtcc.repository;
+
+import com.github.nmsilos.backendtcc.models.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
