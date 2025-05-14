088c376c46568b3db6675bf0598ed2b6305309ce
Nicolas <nicolas.marsil02@gmail.com>
2025-05-14 15:56:29 -0300

    config: first database and pom configs

--- backend-tcc/src/main/java/com/github/nmsilos/backendtcc/BackendTccApplication.java
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/BackendTccApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendTccApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendTccApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTccApplication.class, args);
+    }
 
 }
