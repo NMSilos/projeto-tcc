7f369b0eaf45edc15c167a7b3d98012b55c02830
Nicolas <nicolas.marsil02@gmail.com>
2025-05-14 17:25:20 -0300

    feat: add cadastrar method to UsuarioController

--- backend-tcc/src/main/java/com/github/nmsilos/backendtcc/controller/UsuarioController.java
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/controller/UsuarioController.java
@@ -1,4 +1,26 @@
 package com.github.nmsilos.backendtcc.controller;
 
+import com.github.nmsilos.backendtcc.model.Usuario;
+import com.github.nmsilos.backendtcc.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @PostMapping("cadastrar")
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        repository.save(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
 }
