c29f6e6f05b2f5742d8ff3c220e454ac559e48f3
Nicolas <nicolas.marsil02@gmail.com>
2025-05-14 15:35:45 -0300

    feat: first commit

---
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/BackendTccApplication.java
@@ -0,0 +1,13 @@
+package com.github.nmsilos.backendtcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTccApplication.class, args);
+	}
+
+}
---
+++ backend-tcc/src/test/java/com/github/nmsilos/backendtcc/BackendTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nmsilos.backendtcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
