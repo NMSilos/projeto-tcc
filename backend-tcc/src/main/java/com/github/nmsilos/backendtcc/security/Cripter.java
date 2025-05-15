a789207fe293edb632b23caa17b40c7f5f63dc33
Nicolas <nicolas.marsil02@gmail.com>
2025-05-15 13:49:08 -0300

    config: add Spring Security dependecies

--- backend-tcc/src/main/java/com/github/nmsilos/backendtcc/security/Cripter.java
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/security/Cripter.java
@@ -1,5 +1,7 @@
 package com.github.nmsilos.backendtcc.security;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Cripter {
     public static String criptografar(String texto){
         BCryptPasswordEncoder encoder =
