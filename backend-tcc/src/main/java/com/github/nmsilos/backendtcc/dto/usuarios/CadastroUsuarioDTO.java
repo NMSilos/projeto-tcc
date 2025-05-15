f477a2b4c27e3d89d689d893e1053e4ccd542ef9
Nicolas <nicolas.marsil02@gmail.com>
2025-05-15 14:01:40 -0300

    feat: add CadastroUsuarioDTO

---
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/dto/usuarios/CadastroUsuarioDTO.java
@@ -0,0 +1,41 @@
+package com.github.nmsilos.backendtcc.dto.usuarios;
+
+public class CadastroUsuarioDTO {
+
+    private String nome;
+    private String apelido;
+    private String email;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
