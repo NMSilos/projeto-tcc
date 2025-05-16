ccf42ecadcb5198cafcbcce332deb2354a2bddd9
Nicolas <nicolas.marsil02@gmail.com>
2025-05-16 17:30:29 -0300

    feat: method findByEmail

--- backend-tcc/src/main/java/com/github/nmsilos/backendtcc/repository/UsuarioRepository.java
+++ backend-tcc/src/main/java/com/github/nmsilos/backendtcc/repository/UsuarioRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     public Usuario findByUsername(String username);
+    public Usuario findByEmail(String email);
 }
