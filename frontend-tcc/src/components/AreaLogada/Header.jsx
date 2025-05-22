bfe38b247d80682651b7f04224599bfecf45d90b
Nicolas <nicolas.marsil02@gmail.com>
2025-05-22 13:58:56 -0300

    feat: add search bar

--- frontend-tcc/src/components/AreaLogada/Header.jsx
+++ frontend-tcc/src/components/AreaLogada/Header.jsx
@@ -1,20 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./AreaLogada.css";
 
 export default function Header() {
+
+  const { user } = useParams();
+
   return (
     <header className="header">
       <div className="header-container">
-        <h1 className="logo">AppTCC</h1>
-        <nav className="nav-links">
-          <Link to="/perfil">Perfil</Link>
-          <button className="sair-btn" onClick={() => {
-            localStorage.removeItem("token");
-            window.location.href = "/";
-          }}>
-            Sair
-          </button>
-        </nav>
+        <div className="esquerda">
+          <h1 className="logo">AppTCC</h1>
+        </div>
+        <div className="centro">
+          <input type="text" className="barra-pesquisa" placeholder="🔍︎ Pesquisar" />
+        </div>
+        <div className="direita">
+          <nav className="nav-links">
+            <Link to={`/perfil/${user}`}>Perfil</Link>
+            <button className="sair-btn" onClick={() => {
+              localStorage.removeItem("token");
+              window.location.href = "/";
+            }}>
+              Sair
+            </button>
+          </nav>
+        </div>
       </div>
     </header>
   );
