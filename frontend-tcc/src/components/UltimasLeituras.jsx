3582d68ec3caf0597510d37dc395ba980dd11b69
Nicolas <nicolas.marsil02@gmail.com>
2025-05-23 15:47:21 -0300

    feat: add dinamic rating and comentario to UltimasLeituras

--- frontend-tcc/src/components/UltimasLeituras.jsx
+++ frontend-tcc/src/components/UltimasLeituras.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function UltimasLeituras({ leitura }) {
     const livro = leitura.livro;
+    const comentario = leitura.comentario;
+    const estrelas = [1,2,3,4,5];
     
     if(!livro) return null;
 
@@ -10,11 +12,20 @@ export default function UltimasLeituras({ leitura }) {
       <div className="leitura-conteudo">
         <div className="leitura-img">
           <p>{livro.titulo}</p>
-          <div className="stars">★ ★ ★ ★ ☆</div>
+          <div className="stars">
+            {
+              estrelas.map(item => {
+                if(item <= comentario.nota){
+                  return "★";
+                }
+                return "☆"; 
+              })
+            }
+          </div>
         </div>
         <div className="leitura-review">
           <h3>Review:</h3>
-          <p>DSJFIOJIOASDFJ</p>
+          <p>{comentario.texto}</p>
         </div>
       </div>
     </div>
